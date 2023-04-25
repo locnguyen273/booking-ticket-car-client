3a241a287c96cedfd871d94ce0e6fb84e8fa17ed
Loc Nguyen <tanlocit20062000@gmail.com>
2023-04-25 23:25:09 +0700

    add ui

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import {
 import { history } from "./utils/history";
 import UserTemplate from "./templates/user";
 import Home from "./pages/home";
+import News from "./pages/news";
+import Contact from "./pages/contact";
 
 function App() {
   return (
@@ -15,9 +17,9 @@ function App() {
           <Route element={<UserTemplate />}>
             <Route path="/" element={<Home />} />
             <Route path="/lich-trinh" element={<Home />} />
-            <Route path="/tin-tuc" element={<Home />} />
+            <Route path="/tin-tuc" element={<News />} />
             <Route path="/tuyen-dung" element={<Home />} />
-            <Route path="/lien-he" element={<Home />} />
+            <Route path="/lien-he" element={<Contact />} />
             <Route path="/ve-chung-toi" element={<Home />} />
           </Route>
         </Routes>
